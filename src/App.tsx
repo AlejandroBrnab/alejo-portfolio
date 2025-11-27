f39d1670255dbe175e4c5d702e729c8be69dcd97
AlejandroBrnab <alejandro.rodriguez41104@gmail.com>
2025-11-26 19:16:45 -0500

    changed to dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './components/Header';
+// import Header from './components/Header';
 import Footer from './components/Footer';
 import { ProjectsSection } from './components/ProjectsSection';
 import HeroSection from './components/HeroSection';
 
 const App: React.FC = () => (
   <div>
-    <Header />
+    {/* <Header /> */}
 
     <HeroSection />
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,32 +1,26 @@
 import React from 'react';
 import { Container, Text, Group, ActionIcon } from '@mantine/core';
-import { IconBrandYoutube, IconBrandLinkedin } from '@tabler/icons-react';
+import { IconBrandLinkedin, IconBrandGithub } from '@tabler/icons-react';
 import classes from './Footer.module.css';
 
 const Footer: React.FC = () => (
   <div className={classes.footer}>
     <Container className={classes.inner}>
-      {/* Left side: portfolio name or logo */}
-      <Text fw={700} fz="lg">
-        Alejandro Rodriguez
+
+      {/* copyright */}
+      <Text fz="sm" c="dimmed" ta="center">
+        &copy; {new Date().getFullYear()} Alejandro Bernabe Rodriguez.
       </Text>
 
       {/* Right side: social icons */}
       <Group gap="sm" className={classes.links} justify="flex-end" align="center" wrap="nowrap">
-        <ActionIcon size="lg" color="gray" variant="subtle" component="a" href="https://youtube.com" target="_blank">
-          <IconBrandYoutube size={20} stroke={1.5} />
+        <ActionIcon size="lg" color="gray" variant="subtle" component="a" href="https://github.com" target="_blank">
+          <IconBrandGithub size={20} stroke={1.5} />
         </ActionIcon>
         <ActionIcon size="lg" color="gray" variant="subtle" component="a" href="https://linkedin.com" target="_blank">
           <IconBrandLinkedin size={20} stroke={1.5} />
         </ActionIcon>
       </Group>
-    </Container>
-
-    {/* Bottom copyright */}
-    <Container className={classes.bottom}>
-      <Text fz="sm" c="dimmed" ta="center">
-        &copy; {new Date().getFullYear()} Alejandro Rodriguez. All rights reserved.
-      </Text>
     </Container>
   </div>
 );
